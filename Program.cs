4e1d08c19e07a1e4199d90a5512b00c1c378066a
Aven <misteraveso@gmail.com>
2021-07-28 22:07:14 -0400

    Starting with FPS counter and launching through vscode

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+long timeSinceLastCallMS = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+GameState gameState = GameState.NOT_RUNNING;
+
+int fpsVal; 
+Stopwatch fpsStopwatch = new();
+fpsStopwatch.Start();
+do {
+    fpsVal = (int)Math.Round(1.0 / ((int)fpsStopwatch.ElapsedTicks / 10000000.0));
+    fpsStopwatch.Restart();
+   // var timeDiff = DateTimeOffset.Now.ToUnixTimeMilliseconds() - timeSinceLastCallMS;
+   // fpsVal = (int)Math.Round( 1.0 / ( ((int)timeDiff == 0 ? 1000.0 : (int)timeDiff) / 1000.0 ));
+    Console.Write($"\rFPS: {fpsVal}    ");
+    //ConsoleKeyInfo key = Console.ReadKey(true);
+    //Task.Run(GetNextInput);
+    //timeSinceLastCallMS = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+    await Task.Delay(60 / fpsVal);
+
+} while(true);
+
+/*static Task<ConsoleKeyInfo> GetNextInput() {
+    
+}*/
+
+enum GameState {
+    ERROR = -1,
+    NOT_RUNNING,
+    RUNNING
+}
