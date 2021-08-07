6590735d2bb479d16632070a48088b3a9f3dff40
Aven <misteraveso@gmail.com>
2021-08-07 03:55:57 -0400

    Adding more modular menu system
    Removing FPS system in place for manual entry.
    Adding config.
    Adding JSON and data.

--- Program.cs
+++ Program.cs
@@ -1,33 +1,103 @@
-﻿using System;
-using System.Threading.Tasks;
-using System.Threading;
+﻿using System.Text;
+using System.Text.Json;
 using System.Diagnostics;
+using System.IO;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
 
-long timeSinceLastCallMS = DateTimeOffset.Now.ToUnixTimeMilliseconds();
-GameState gameState = GameState.NOT_RUNNING;
+const int FPS = 60;
 
-int fpsVal; 
-Stopwatch fpsStopwatch = new();
-fpsStopwatch.Start();
-do {
-    fpsVal = (int)Math.Round(1.0 / ((int)fpsStopwatch.ElapsedTicks / 10000000.0));
-    fpsStopwatch.Restart();
-   // var timeDiff = DateTimeOffset.Now.ToUnixTimeMilliseconds() - timeSinceLastCallMS;
-   // fpsVal = (int)Math.Round( 1.0 / ( ((int)timeDiff == 0 ? 1000.0 : (int)timeDiff) / 1000.0 ));
-    Console.Write($"\rFPS: {fpsVal}    ");
-    //ConsoleKeyInfo key = Console.ReadKey(true);
-    //Task.Run(GetNextInput);
-    //timeSinceLastCallMS = DateTimeOffset.Now.ToUnixTimeMilliseconds();
-    await Task.Delay(60 / fpsVal);
+//////////////////// START 
 
-} while(true);
+/// CONFIG START
+Config config;
+Console.Write("Loading...");
+string dataFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "AdvancedPong");
+if(!Directory.Exists(dataFolder)) Directory.CreateDirectory(dataFolder);
+dataFolder = Path.Combine(dataFolder, "save.dat");
+if(File.Exists(dataFolder)) {
+    var fileDataString = File.ReadAllText(dataFolder);
+    config = JsonSerializer.Deserialize<Config>(fileDataString);
+} else {
+    config = new();
+    File.WriteAllText(dataFolder, JsonSerializer.Serialize(config));
+}
+/// CONFIG END
 
-/*static Task<ConsoleKeyInfo> GetNextInput() {
+ShowMenu(ref config);
+
+//////////////////// END
+
+static void ShowMenu(ref Config config) {
+    Console.Clear();
+    Console.WriteLine("----------------------------------------------------------------");
+    Console.WriteLine("      :::::::::       ::::::::       ::::    :::       :::::::: ");
+    Console.WriteLine("     :+:    :+:     :+:    :+:      :+:+:   :+:      :+:    :+: ");
+    Console.WriteLine("    +:+    +:+     +:+    +:+      :+:+:+  +:+      +:+         ");
+    Console.WriteLine("   +#++:++#+      +#+    +:+      +#+ +:+ +#+      :#:          ");
+    Console.WriteLine("  +#+            +#+    +#+      +#+  +#+#+#      +#+   +#+#    ");
+    Console.WriteLine(" #+#            #+#    #+#      #+#   #+#+#      #+#    #+#     ");
+    Console.WriteLine("###             ########       ###    ####       ########       ");
+    Console.WriteLine("----------------------------------------------------------------\n");
+    Console.WriteLine("[1] Start Pong");
+    Console.WriteLine("[2] Options");
+    Console.WriteLine("[3] Quit");
+    ConsoleKey userInput = SelectOption(ConsoleKey.D1, ConsoleKey.D2, ConsoleKey.D3);
+    switch(userInput) {
+        case ConsoleKey.D1:
+            RunGame(ref config);
+            break;
+        case ConsoleKey.D2:
+            ShowOptions(ref config);
+            break;
+        case ConsoleKey.D3:
+            Environment.Exit(0);
+            break;
+        default:
+            Environment.Exit(0);
+            break;
+    }
+}
+
+static void ShowOptions(ref Config config) {
     
-}*/
+}
 
+static void RunGame(ref Config config) {
+
+    // SET UP GAME WITH CONFIG
+
+    Stopwatch timer = new();
+    timer.Start();
+    do {
+        if(timer.ElapsedTicks > FPS * 10) {
+            timer.Restart();
+            // GET USER INPUT
+            // GET AI INPUT
+            // UPDATE MOVEMENT
+            // 
+        }
+    } while(true);   
+}
+
+static ConsoleKey SelectOption(params ConsoleKey[] acceptableKeys) {
+    ConsoleKeyInfo userInput;
+    do {
+        userInput = Console.ReadKey(true);
+        if(userInput.Key == ConsoleKey.Escape)
+            Environment.Exit(0);
+    } while(acceptableKeys.All(key => key != userInput.Key));
+    return userInput.Key;
+}
 enum GameState {
     ERROR = -1,
     NOT_RUNNING,
     RUNNING
+}
+
+class Config {
+    public int Width {get; set;}  = 100;
+    public int Height {get; set;} = 30;
+
 }
